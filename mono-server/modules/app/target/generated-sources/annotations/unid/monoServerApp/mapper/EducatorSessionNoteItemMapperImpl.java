1e42085ff2f5bd85abbc0c59b2955c63d542352d
linjun <1019193566@qq.com>
2024-03-16 23:39:30 +0800

    educator part 1. session comment

---
+++ mono-server/modules/app/target/generated-sources/annotations/unid/monoServerApp/mapper/EducatorSessionNoteItemMapperImpl.java
@@ -0,0 +1,50 @@
+package unid.monoServerApp.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import unid.monoServerApp.database.table.educatorSessionNote.DbEducatorSessionNoteItem;
+import unid.monoServerMeta.api.EducatorSessionNoteCommentResponse;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-03-16T23:25:49+0800",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.20.1 (Amazon.com Inc.)"
+)
+@Component
+public class EducatorSessionNoteItemMapperImpl implements EducatorSessionNoteItemMapper {
+
+    @Autowired
+    private I18nMapper i18nMapper;
+
+    @Override
+    public EducatorSessionNoteCommentResponse toResponse(DbEducatorSessionNoteItem.Result data) {
+        if ( data == null ) {
+            return null;
+        }
+
+        EducatorSessionNoteCommentResponse educatorSessionNoteCommentResponse = new EducatorSessionNoteCommentResponse();
+
+        educatorSessionNoteCommentResponse.setId( data.getId() );
+        educatorSessionNoteCommentResponse.setTitleI18n( i18nMapper.toModel( data.getTitleI18n() ) );
+        educatorSessionNoteCommentResponse.setComment( data.getComment() );
+
+        return educatorSessionNoteCommentResponse;
+    }
+
+    @Override
+    public List<EducatorSessionNoteCommentResponse> toResponse(List<DbEducatorSessionNoteItem.Result> data) {
+        if ( data == null ) {
+            return null;
+        }
+
+        List<EducatorSessionNoteCommentResponse> list = new ArrayList<EducatorSessionNoteCommentResponse>( data.size() );
+        for ( DbEducatorSessionNoteItem.Result result : data ) {
+            list.add( toResponse( result ) );
+        }
+
+        return list;
+    }
+}
---
+++ mono-server/modules/app/target/generated-sources/annotations/unid/monoServerApp/mapper/StudentPaymentTransactionMapperImpl.java
@@ -0,0 +1,42 @@
+package unid.monoServerApp.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+import unid.jooqMono.model.tables.pojos.StudentPaymentTransactionPojo;
+import unid.monoServerMeta.api.StudentBookingEducatorCalendarRequest;
+import unid.monoServerMeta.api.StudentPaymentTransactionResponse;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-03-16T23:25:51+0800",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.20.1 (Amazon.com Inc.)"
+)
+@Component
+public class StudentPaymentTransactionMapperImpl implements StudentPaymentTransactionMapper {
+
+    @Override
+    public StudentPaymentTransactionPojo toPojo(StudentBookingEducatorCalendarRequest request) {
+        if ( request == null ) {
+            return null;
+        }
+
+        StudentPaymentTransactionPojo studentPaymentTransactionPojo = new StudentPaymentTransactionPojo();
+
+        studentPaymentTransactionPojo.setTransactionItemRefId( request.getEducatorCalendarId() );
+
+        return studentPaymentTransactionPojo;
+    }
+
+    @Override
+    public StudentPaymentTransactionResponse toResponse(StudentPaymentTransactionPojo pojo) {
+        if ( pojo == null ) {
+            return null;
+        }
+
+        StudentPaymentTransactionResponse studentPaymentTransactionResponse = new StudentPaymentTransactionResponse();
+
+        studentPaymentTransactionResponse.setId( pojo.getId() );
+
+        return studentPaymentTransactionResponse;
+    }
+}
